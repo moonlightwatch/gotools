e28713e1d976f76bb1d460640f77acf64ec51c14
git <git@localhost>
2023-12-21 13:18:19 +0000

    test: 增加单元测试

--- collections/list_test.go
+++ collections/list_test.go
@@ -8,8 +8,7 @@ import (
 
 func TestList(t *testing.T) {
 
-	// 测试List
-	{
+	{ // 测试List IntComparable
 		l := collections.NewList[collections.IntComparable]()
 		l.Add(1)
 		l.Add(2)
@@ -79,4 +78,75 @@ func TestList(t *testing.T) {
 		}
 
 	}
+	{ // 测试List StringComparable
+		l := collections.NewList[collections.StringComparable]()
+		l.Add("1")
+		l.Add("2")
+		l.Add("3")
+		if l.Size() != 3 {
+			t.Errorf("List.Size() failed. l.Size(): %d", l.Size())
+		}
+		if !l.Contains("1") {
+			t.Errorf("List.Contains() failed. l.Contains(1): %t", l.Contains("1"))
+		}
+		if !l.Contains("2") {
+			t.Errorf("List.Contains() failed. l.Contains(2): %t", l.Contains("2"))
+		}
+		if !l.Contains("3") {
+			t.Errorf("List.Contains() failed. l.Contains(3): %t", l.Contains("3"))
+		}
+		if l.Contains("4") {
+			t.Errorf("List.Contains() failed. l.Contains(4): %t", l.Contains("4"))
+		}
+		l.Remove("1")
+		if l.Size() != 2 {
+			t.Errorf("List.Size() failed. l.Size(): %d", l.Size())
+		}
+		if l.Contains("1") {
+			t.Errorf("List.Contains() failed. l.Contains(1): %t", l.Contains("1"))
+		}
+		if !l.Contains("2") {
+			t.Errorf("List.Contains() failed. l.Contains(2): %t", l.Contains("2"))
+		}
+		if !l.Contains("3") {
+			t.Errorf("List.Contains() failed. l.Contains(3): %t", l.Contains("3"))
+		}
+		if l.Contains("4") {
+			t.Errorf("List.Contains() failed. l.Contains(4): %t", l.Contains("4"))
+		}
+		l.Remove("2")
+		if l.Size() != 1 {
+			t.Errorf("List.Size() failed. l.Size(): %d", l.Size())
+		}
+		if l.Contains("1") {
+			t.Errorf("List.Contains() failed. l.Contains(1): %t", l.Contains("1"))
+		}
+		if l.Contains("2") {
+			t.Errorf("List.Contains() failed. l.Contains(2): %t", l.Contains("2"))
+		}
+		if !l.Contains("3") {
+			t.Errorf("List.Contains() failed. l.Contains(3): %t", l.Contains("3"))
+		}
+		if l.Contains("4") {
+			t.Errorf("List.Contains() failed. l.Contains(4): %t", l.Contains("4"))
+		}
+		l.Remove("3")
+		if l.Size() != 0 {
+			t.Errorf("List.Size() failed. l.Size(): %d", l.Size())
+		}
+		if l.Contains("1") {
+			t.Errorf("List.Contains() failed. l.Contains(1): %t", l.Contains("1"))
+		}
+		if l.Contains("2") {
+			t.Errorf("List.Contains() failed. l.Contains(2): %t", l.Contains("2"))
+		}
+		if l.Contains("3") {
+			t.Errorf("List.Contains() failed. l.Contains(3): %t", l.Contains("3"))
+		}
+		if l.Contains("4") {
+			t.Errorf("List.Contains() failed. l.Contains(4): %t", l.Contains("4"))
+		}
+
+	}
+
 }
